e5708345f1c25ff32ed2a3124786c4775b30f81b
Mahdi <mahdimey17@gmail.com>
2023-11-23 01:02:30 +0330

    fixed touch device bug

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const blob = document.querySelector('.blob')
 const blur = document.querySelector('.blur')
 
-document.addEventListener('mousemove', e => {
-    const { clientX, clientY } = e
+// Check if the device has touch capabilities
+const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+console.log(isTouchDevice)
 
-    blob.style.left = `${clientX}px`
-    blob.style.top = `${clientY}px`
+// Only attach the event listener if it's not a touch device
+if (!isTouchDevice) {
+    document.addEventListener('mousemove', e => {
+        const { clientX, clientY } = e;
 
-})
+        blob.style.left = `${clientX}px`;
+        blob.style.top = `${clientY}px`;
+    });
+}
